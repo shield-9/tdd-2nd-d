dc69acc4b88c918effe3b4f5f03d2cf1de855b13
Daisuke Takahashi <daisuke_takahashi@cyberagent.co.jp>
2017-09-04 18:27:13 +0900

    Local part

--- validate_addr_spec.py
+++ validate_addr_spec.py
@@ -8,22 +8,55 @@ def validate_addr_spec(addr):
     local, domain = email[0], email[1]
 
     # domain part
-    if domain.strip('.') is not domain:
+    if not check_dot_atom(domain):
         return 'ng'
 
-    if len(domain) == 0:
+    # local part
+    if local.find('"') == -1:
+        if not check_dot_atom(local):
+            return 'ng'
+    elif not check_quoted_string(local):
         return 'ng'
+
+    return 'ok'
+
+def check_dot_atom(text):
+    if text.strip('.') is not text:
+        return False
+
+    if len(text) == 0:
+        return False
     
-    if domain.find('..') != -1:
-        return 'ng'
+    if text.find('..') != -1:
+        return False
 
     ng_chars = re.escape(r"!#$%&'*+-/=?^_`{|}~.")
     pattern = "[^0-9a-zA-Z%s]" % ng_chars
 
-    if re.compile(pattern).search(domain):
-        return 'ng'
+    if re.compile(pattern).search(text):
+        return False
 
-    return 'ok'
+    return True
+
+def check_quoted_string(text):
+    if not text.startswith('"'):
+        return False
+
+    if not text.endswith('"'):
+        return False
+
+    if len(text) < 2:
+        return False
+
+    text = text.strip('"')
+
+    ng_chars = re.escape(r"!#$%&'*+-/=?^_`{|}~(),.:;<>@[]\"\\")
+    pattern = "[^0-9a-zA-Z%s]" % ng_chars
+
+    if re.compile(pattern).search(text):
+        return False
+    
+    return True
 
 if __name__ == '__main__':
     validate_addr_spec('abc@example.com')
